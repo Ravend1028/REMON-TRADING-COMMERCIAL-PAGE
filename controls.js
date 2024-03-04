c29608f00bede4446c2c98385fe8a390d88706ad
Ravend1028 <ravendavid.rd30@gmail.com>
2024-03-04 16:16:00 +0800

    optimized mobile viewport

--- controls.js
+++ controls.js
@@ -1,6 +1,18 @@
 var copy = document.querySelector(".icons_container").cloneNode(true);
 document.querySelector(".sliding_icons").appendChild(copy);
 
+const hamburger = document.querySelector(".burger_menu");
+const headNav = document.querySelector(".header_nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  headNav.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav_li").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  headNav.classList.remove("active");
+}))
 /*
 function slideContainer() {
   const productContainers = document.querySelectorAll('.cards_container');
